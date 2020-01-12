1ed0452983f1b7899a1ebf60826cbc8759e39e2b
gtkatakura <gt.katakura@gmmail.com>
2020-01-12 17:31:39 -0300

    Refactoring code for normal item

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,7 +1,7 @@
 const { Shop, Item } = require('../src/gilded_rose')
 
 describe('Gilded Rose', () => {
-  describe('when is a normal item', () => {
+  describe.only('when is a normal item', () => {
     describe('and sellIn is greater than 0', () => {
       it('decrements quality and sellIn in 1', () => {
         const gildedRose = new Shop([new Item('+5 Dexterity Vest', 10, 20)])
@@ -18,7 +18,7 @@ describe('Gilded Rose', () => {
 
     describe('and sellIn is equal 0', () => {
       describe('and quality is greater than 0', () => {
-        it('decrements quality and sellIn in 1', () => {
+        it('decrements quality and sellIn in 2', () => {
           const gildedRose = new Shop([new Item('+5 Dexterity Vest', 0, 20)])
 
           const items = gildedRose.updateQuality()
